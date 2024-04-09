49e5a088ce0ab47aca49095087398b392ddf0306
srinivas3p <87357998+srinivas3p@users.noreply.github.com>
2024-04-08 23:31:42 -0500

    UI code Srinivas Peeka Group 5 11.31 PM 4/8/2024

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let signup = document.querySelector(".signup");
+let sp = document.querySelector(".signup");
 let login = document.querySelector(".login");
-let slider = document.querySelector(".slider");
+let sl = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
 
-signup.addEventListener("click", () => {
-	slider.classList.add("moveslider");
+sp.addEventListener("click", () => {
+	sl.classList.add("moveslider");
 	formSection.classList.add("form-section-move");
 });
 
 login.addEventListener("click", () => {
-	slider.classList.remove("moveslider");
+	sl.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
 });
